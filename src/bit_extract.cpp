85b1dc18cce1252cf3372f13f0fd343190cf2d85
jasbury1 <jamesasbury03@gmail.com>
2021-05-08 21:55:41 -0700

    Hip util header added

--- src/bit_extract.cpp
+++ src/bit_extract.cpp
@@ -25,16 +25,7 @@ THE SOFTWARE.
 #include "hip/hip_runtime.h"
 
 #include "GPUKernels.h"
-
-#define CHECK(cmd)                                                                                 \
-    {                                                                                              \
-        hipError_t error = cmd;                                                                    \
-        if (error != hipSuccess) {                                                                 \
-            fprintf(stderr, "error: '%s'(%d) at %s:%d\n", hipGetErrorString(error), error,         \
-                    __FILE__, __LINE__);                                                           \
-            exit(EXIT_FAILURE);                                                                    \
-        }                                                                                          \
-    }
+#include "millipyde_hip_util.h"
 
 __global__ void bit_extract_kernel(uint32_t* C_d, const uint32_t* A_d, size_t N) {
     size_t offset = (hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x);
@@ -72,28 +63,28 @@ int run_bit_extract() {
 #endif
 
     int deviceId;
-    CHECK(hipGetDevice(&deviceId));
+    HIP_CHECK(hipGetDevice(&deviceId));
     hipDeviceProp_t props;
-    CHECK(hipGetDeviceProperties(&props, deviceId));
+    HIP_CHECK(hipGetDeviceProperties(&props, deviceId));
     printf("info: running on device #%d %s\n", deviceId, props.name);
 
 
     printf("info: allocate host mem (%6.2f MB)\n", 2 * Nbytes / 1024.0 / 1024.0);
     A_h = (uint32_t*)malloc(Nbytes);
-    CHECK(A_h == 0 ? hipErrorOutOfMemory : hipSuccess);
+    HIP_CHECK(A_h == 0 ? hipErrorOutOfMemory : hipSuccess);
     C_h = (uint32_t*)malloc(Nbytes);
-    CHECK(C_h == 0 ? hipErrorOutOfMemory : hipSuccess);
+    HIP_CHECK(C_h == 0 ? hipErrorOutOfMemory : hipSuccess);
 
     for (size_t i = 0; i < N; i++) {
         A_h[i] = i;
     }
 
     printf("info: allocate device mem (%6.2f MB)\n", 2 * Nbytes / 1024.0 / 1024.0);
-    CHECK(hipMalloc(&A_d, Nbytes));
-    CHECK(hipMalloc(&C_d, Nbytes));
+    HIP_CHECK(hipMalloc(&A_d, Nbytes));
+    HIP_CHECK(hipMalloc(&C_d, Nbytes));
 
     printf("info: copy Host2Device\n");
-    CHECK(hipMemcpy(A_d, A_h, Nbytes, hipMemcpyHostToDevice));
+    HIP_CHECK(hipMemcpy(A_d, A_h, Nbytes, hipMemcpyHostToDevice));
 
     printf("info: launch 'bit_extract_kernel' \n");
     const unsigned blocks = 512;
@@ -101,7 +92,7 @@ int run_bit_extract() {
     hipLaunchKernelGGL(bit_extract_kernel, dim3(blocks), dim3(threadsPerBlock), 0, 0, C_d, A_d, N);
 
     printf("info: copy Device2Host\n");
-    CHECK(hipMemcpy(C_h, C_d, Nbytes, hipMemcpyDeviceToHost));
+    HIP_CHECK(hipMemcpy(C_h, C_d, Nbytes, hipMemcpyDeviceToHost));
 
     printf("info: check result\n");
     for (size_t i = 0; i < N; i++) {
@@ -109,7 +100,7 @@ int run_bit_extract() {
         if (C_h[i] != Agold) {
             fprintf(stderr, "mismatch detected.\n");
             printf("%zu: %08x =? %08x (Ain=%08x)\n", i, C_h[i], Agold, A_h[i]);
-            CHECK(hipErrorUnknown);
+            HIP_CHECK(hipErrorUnknown);
         }
     }
     printf("PASSED!\n");
@@ -130,9 +121,9 @@ int add_one(void * data, int N)
     size_t Nbytes = N * sizeof(long);
     long *data_d;
 
-    CHECK(hipMalloc(&data_d, Nbytes));
+    HIP_CHECK(hipMalloc(&data_d, Nbytes));
     printf("info: copy Host2Device\n");
-    CHECK(hipMemcpy(data_d, data, Nbytes, hipMemcpyHostToDevice));
+    HIP_CHECK(hipMemcpy(data_d, data, Nbytes, hipMemcpyHostToDevice));
 
     const unsigned blocks = 512;
     const unsigned threadsPerBlock = 256;
@@ -140,7 +131,7 @@ int add_one(void * data, int N)
     hipLaunchKernelGGL(add_one_kernel, dim3(blocks), dim3(threadsPerBlock), 0, 0, data_d, N);
     
     printf("info: copy Device2Host\n");
-    CHECK(hipMemcpy(data, data_d, Nbytes, hipMemcpyDeviceToHost));
+    HIP_CHECK(hipMemcpy(data, data_d, Nbytes, hipMemcpyDeviceToHost));
     return 0;
 }
 
---
+++ src/include/millipyde_hip_util.h
@@ -0,0 +1,19 @@
+#ifndef MILLIPYDE_HIP_UTIL_H
+#define MILLIPYDE_HIP_UTIL_H
+
+#include <stdio.h>
+#include <iostream>
+#include "hip/hip_runtime.h"
+
+#define HIP_CHECK(cmd)                                                         \
+    {                                                                          \
+        hipError_t error = cmd;                                                \
+        if (error != hipSuccess) {                                             \
+            fprintf(stderr, "error: '%s'(%d) at %s:%d\n",                      \
+                    hipGetErrorString(error), error,                           \
+                    __FILE__, __LINE__);                                       \
+            exit(EXIT_FAILURE);                                                \
+        }                                                                      \
+    }
+
+#endif //MILLIPYDE_HIP_UTIL_H
