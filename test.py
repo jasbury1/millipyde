0b33554a0f2a94c102d33b57a7ade8e0c48ba7c1
jasbury1 <jamesasbury03@gmail.com>
2021-05-16 23:30:58 -0700

    better test script

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import numpy as np
 from skimage import data, io, filters
 from skimage.io import imsave, imread
-from skimage.color import rgb2gray
+from skimage.color import rgb2gray, rgba2rgb
+
 import timeit
+import time
 
 import millipyde as mp
 
@@ -31,19 +33,30 @@ gpuimg = mp.GPUArray(image)
 imsave("test.png", image)
 
 '''
-img = data.chelsea()
-gpuimg = mp.GPUArray(img)
-gpuimg.to_greyscale()
-imsave("chelsea_original.png", img)
-imsave("chelsea_grey.png", gpuimg.__array__())
-
-
 
 charlie = io.imread("examples/images/charlie.png")
 charlie_on_gpu = mp.GPUArray(charlie)
+
+start = time.perf_counter()
 charlie_on_gpu.to_greyscale()
-imsave("output/charlie_original.png", charlie)
+stop = time.perf_counter()
+print("\nTime to calculate: {}\n".format(stop - start))
+
+start = time.perf_counter()
 imsave("output/charlie_grey.png", charlie_on_gpu.__array__())
+stop = time.perf_counter()
+print("\nTime to save: {}\n".format(stop - start))
+
+start = time.perf_counter()
+grey_charlie = rgb2gray(rgb2gray(rgba2rgb(charlie)))
+stop = time.perf_counter()
+print("\nSKImage time to calculate: {}\n".format(stop - start))
+
+start = time.perf_counter()
+imsave("output/charlie_grey_skimage.png", grey_charlie)
+stop = time.perf_counter()
+print("\nSKImage time to save: {}\n".format(stop - start))
+
 
 
 
