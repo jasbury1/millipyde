564e374269edf4e601e5ef3aea87ce1d003076e8
jasbury1 <jamesasbury03@gmail.com>
2021-05-26 16:55:26 -0700

    better bash script for running tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,13 +4,56 @@ RED='\033[0;31m'
 CYAN='\033[0;36m'
 NC='\033[0m' # No Color
 
-echo -e "\n${CYAN}Running Python Unittests...${NC}\n"
+rebuild=false
+run_python=true
+run_backend=true
 
-python3 tests/millipyde_tests.py -v
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo "Runs the millipyde test suite"
+      echo " "
+      echo "options:"
+      echo "-b, --build               rebuild before running"
+      echo "-p, --python              run only the python tests"
+      echo "-c, --c                   run only the c/c++ tests"
+      exit 0
+      ;;
+    -b|--build)
+      rebuild=true
+      shift
+      ;;
+    -p|--python)
+      run_backend=false
+      shift
+      ;;
+    -c|--c)
+      run_python=false
+      shift
+      ;;
+  esac
+done
 
-echo -e "\n${CYAN}Running Backend Unittests...${NC}\n"
+if  [ "$run_python" = true ] ; then
+  if  [ "$rebuild" = true ] ; then
+    ./build.sh
+  fi
+  echo -e "\n${CYAN}Running Python Unittests...${NC}\n"
+  python3 tests/millipyde_tests.py -v
+fi
 
-cd ./tests/backend/
-make clean
-make test
+if  [ "$run_backend" = true ] ; then
+  if  [ "$rebuild" = true ] ; then
+    cd ./tests/backend/
+    make clean
+    make test
+    cd -
+  fi
+  echo -e "\n${CYAN}Running Backend Unittests...${NC}\n"
+fi
+
+
+
+
+      
 
